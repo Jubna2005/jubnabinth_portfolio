55c6e5e748c1f5358a445a773e819dcb725d9504
Jubna <jubna2005@gmail.com>
2026-07-14 20:59:01 +0530

    fix homepage

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jubnabinth_portfolio/",
+  base: '/jubnabinth_portfolio/', // 👈 VERY IMPORTANT
 })
